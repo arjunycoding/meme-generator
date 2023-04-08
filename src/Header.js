9982155fe6b70c18508efe1769e17b8722871006
Arjun Yuvaraj <arjunycoding@gmail.com>
2023-04-08 15:39:52 -0400

    fixed

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import logo from "./images/logo.png"
 export default function Header() {
     return (
         <div className="header">
-            {/* <img src={logo} alt="meme-troll" className="header--image " /> */}
+            <img src={logo} alt="meme-troll" className="header--image " />
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--">Arjun Yuvaraj</h4>
         </div>
